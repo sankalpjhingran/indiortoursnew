9872e8284a47f4e3ff2af3cd93c1f09ea0f98d99
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2017-09-07 01:54:42 -0700

    Update

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -218,7 +218,7 @@ module.exports = function (grunt) {
     // Automatically inject Bower components into the app
     wiredep: {
       app: {
-        src: ['<%= yeoman.app %>/index.html'],
+        src: ['<%= yeoman.app %>/index.html', '<%= yeoman.app %>/views/about.html',],
         ignorePath:  /\.\.\//
       },
       test: {
