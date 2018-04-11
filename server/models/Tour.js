e8d187f03c1bb94f479b0156361f4fbf176119a9
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2018-04-11 13:39:11 -0700

    Update Tour and ParentTour models.

--- server/models/Tour.js
+++ server/models/Tour.js
@@ -10,13 +10,13 @@ module.exports = (sequelize, DataTypes) => {
         tourtype: {type: DataTypes.ENUM, values: ['Group', 'Individual', 'Regular'], defaultValue: 'Regular', allowNull: false},
         days: {type: DataTypes.INTEGER},
         nights: {type: DataTypes.INTEGER},
-        price: {type: DataTypes.DECIMAL}
+        price: {type: DataTypes.DECIMAL},
       }
   );
 
   Tour.associate = function(models){
       Tour.hasMany(models.Itinerary, {as: 'itinerary', foreignKey: 'tour_id'}, { onDelete: 'cascade' });
-      Tour.belongsTo(models.ParentTour, {as: 'tourcostdetails', foreignKey: 'parenttour_id'}, { onDelete: 'cascade' });
+      Tour.belongsTo(models.ParentTour, {as: 'tours', foreignKey: 'parenttour_id'});
       Tour.hasMany(models.TourCost, {as: 'tourcost', foreignKey: 'tour_id'}, { onDelete: 'cascade' });
       //Tour.hasMany(models.AdditionalServiceSupplements, {foreignKey: 'tour_id'});
       //Tour.belongsTo(models.MoreInfo, {foreignKey: 'tour_id'});
