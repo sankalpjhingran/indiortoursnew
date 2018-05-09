eded654c1b872da602b4632f0cd49172ceb38d6d
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2018-05-08 18:58:05 -0700

    Add Geolocation data to Location and Places

--- server/models/Location.js
+++ server/models/Location.js
@@ -9,9 +9,28 @@ module.exports = (sequelize, DataTypes) => {
         country:{type: DataTypes.STRING},
         continent:{type: DataTypes.STRING},
         description: {type: DataTypes.TEXT('medium')},
-      },
-      {charset: 'utf8',collate: 'utf8_unicode_ci'}
-  );
+        latitude: {
+          type: DataTypes.DECIMAL(10, 8),
+          allowNull: true,
+          defaultValue: null,
+          validate: { min: -90, max: 90 }
+        },
+        longitude: {
+          type: DataTypes.DECIMAL(11, 8),
+          allowNull: true,
+          defaultValue: null,
+          validate: { min: -180, max: 180 }
+        },
+        elevation: {type: DataTypes.INTEGER},
+      }, {
+      validate: {
+        bothCoordsOrNone() {
+          if ((this.latitude === null) !== (this.longitude === null)) {
+            throw new Error('Require either both latitude and longitude or neither')
+          }
+        }
+      }
+  });
 
   Location.associate = function (models) {
     Location.hasMany(models.Hotel, {as: 'locationhotels', foreignKey: 'location_id'});
