e099f11600298f28d61dd68330a6e951aa8f2d81
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2018-05-22 21:08:25 -0700

    Feature addition
    1. Pagination for Images Admin
    2. Departure dates and calendar display for tour departures.

--- client/app/scripts/controllers/TourViewController.js
+++ client/app/scripts/controllers/TourViewController.js
@@ -8,12 +8,26 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('TourViewController', ['$http','$state', '$rootScope', '$scope', '$stateParams', function ($http, $state, $rootScope, $scope, $stateParams) {
+  .controller('TourViewController', ['$http','$state', '$rootScope', '$scope', '$stateParams', function ($http, $state, $rootScope, $scope, $stateParams, calendarConfig) {
     $rootScope.$state = $state;
 
     var tourId = $stateParams.id;
     $scope.tourWithAllRelated = [];
     var imagesMap = new Map();
+
+    var vm = this;
+    var events = [];
+    vm.calendarView = 'year';
+    vm.viewDate = moment().toDate();
+    vm.events = [];
+    vm.cellIsOpen = true;
+
+    vm.viewChangeClicked = function(nextView) {
+      if (nextView === 'day') {
+        return false;
+      }
+    };
+
 
     $scope.getTourDetailsWithRelatedModels = function(){
       $http.get('/api/tours/tourdetailswithrelatedmodels/', {params: {id: tourId}})
@@ -59,6 +73,60 @@ angular.module('clientApp')
                   }
               });
 
+              $scope.events = [];
+              angular.forEach($scope.tourWithAllRelated[0].departuredates, function(date){
+                // Parse a RRuleSet string, return a RRuleSet object
+                //BYDAY=MO,FR
+
+                  var BYDAY = [];
+                  if(date.repeatfrequency == 'Week') {
+                      console.log(date.repeatondayofweek);
+                      angular.forEach(date.repeatondayofweek.split(','), function(day){
+                          BYDAY.push(day.substring(0, 2).toUpperCase());
+                      })
+                  }
+                  console.log(BYDAY.toString());
+                  //rrulestr('RRULE:BYDAY=' + BYDAY.toString());
+                  var rule = rrulestr('RRULE:BYDAY=' + BYDAY.toString())
+                  console.log(rrulestr('RRULE:BYDAY=' + BYDAY.toString()));
+                  console.log(rule.options.byweekday);
+
+                  $scope.events.push(
+                    {
+                      title: date.title,
+                      color: '#ffa500',
+                      rrule: {
+                        freq: date.repeatfrequency == 'Week' ? RRule.WEEKLY : date.repeatfrequency == 'Month' ? RRule.MONTHLY : date.repeatfrequency == 'Year' ? RRule.YEARLY : RRule.WEEKLY,
+                        bymonthday: date.repeatfrequency == 'Month' ? date.repeatondayofmonth : null,
+                        count: date.repeatendsafteroccurrences,
+                        interval: date.repeatfor,
+                        dtstart: new Date((new Date()).getFullYear(), 1, 1, 8, 0, 0),
+                        until: new Date((new Date()).getFullYear(), 12, 31, 8, 0, 0),
+                        byweekday: date.repeatfrequency == 'Week' ? rule.origOptions.byweekday : null,
+                      }
+                    }
+                  );
+              });
+
+              /*
+              $scope.events = [{
+                title: 'Recurs on the 5th of each month',
+                color: 'yellow',
+                rrule: {
+                  freq: RRule.MONTHLY,
+                  bymonthday: 5
+                }
+              }, {
+                title: 'Recurs weekly on Thursdays',
+                color: 'green',
+                rrule: {
+                  freq: RRule.WEEKLY,
+                  byweekday: [RRule.TH],
+                }
+              }];
+              */
+              console.log('Calling watch function=======>');
+              watchFunction();
            },
            function(response){
              // failure call back
@@ -66,6 +134,58 @@ angular.module('clientApp')
         );
     }
 
+    var watchFunction = function(){
+      // the code
+      console.log('In watch function=======>');
+      vm.events = [];
+
+      if($scope.events && $scope.events.length) {
+          console.log($scope.events);
+          $scope.events.forEach(function(event) {
+
+          // Use the rrule library to generate recurring events: https://github.com/jkbrzt/rrule
+          var rule = new RRule(angular.extend({}, event.rrule, {
+            dtstart: moment(vm.viewDate).startOf(vm.calendarView).toDate(),
+            until: moment(vm.viewDate).endOf(vm.calendarView).toDate()
+          }));
+
+          rule.all().forEach(function(date) {
+            vm.events.push(angular.extend({}, event, {
+              startsAt: new Date(date)
+            }));
+          });
+        })
+        console.log(vm.events);
+      }
+    }
+
+    $scope.$watchGroup([
+      'vm.calendarView',
+      'vm.viewDate'
+    ], watchFunction);
+
+    vm.timespanClicked = function(date, cell) {
+      console.log('Timespan clicked.....');
+      if (vm.calendarView === 'month') {
+        console.log('Month is true');
+        if ((vm.cellIsOpen && moment(date).startOf('day').isSame(moment(vm.viewDate).startOf('day'))) || cell.events.length === 0 || !cell.inMonth) {
+          console.log('=======>1');
+          vm.cellIsOpen = false;
+        } else {
+          console.log('=======>2');
+          vm.cellIsOpen = true;
+          vm.viewDate = date;
+        }
+      } else if (vm.calendarView === 'year') {
+        if ((vm.cellIsOpen && moment(date).startOf('month').isSame(moment(vm.viewDate).startOf('month'))) || cell.events.length === 0) {
+          vm.cellIsOpen = false;
+        } else {
+          vm.cellIsOpen = true;
+          vm.viewDate = date;
+        }
+      }
+
+    };
 
   $scope.oneAtATime = true;
   $scope.status = {
