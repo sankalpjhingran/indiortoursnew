b181acc475df08bf6df220f9ec1257a2e1cca838
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2018-06-18 19:37:52 -0700

    Use random ids when creating costs json.

--- client/app/scripts/controllers/TourCostsController.js
+++ client/app/scripts/controllers/TourCostsController.js
@@ -199,7 +199,7 @@ $scope.ngModelOptionsSelected = function(value) {
   //http://plnkr.co/edit/bDFIP66b5it5Q3KHy1LT?p=preview
   vm.addData = function() {
     $scope.myData.push( {
-        id: $scope.myData.length+1,
+        id: Math.ceil(Math.random() * 10000),
         costcategory: "",
         costitem: "",
         budget: "",
