fc0fdd84c7e4d5f854062caa013f1768c8b5c759
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2018-07-05 16:25:48 -0700

    Add server side security for routes

--- server/routes/hotel.js
+++ server/routes/hotel.js
@@ -4,12 +4,13 @@ var path = require('path');
 var express = require('express');
 var router = express.Router();
 var hotelController  = require('../controllers/hotel');
+var authenticated = require('./authenticated');
 
 console.log('In Hotel route===>');
-router.post('/', hotelController.create);
+router.post('/', authenticated, hotelController.create);
 router.get('/all/', hotelController.index);
 router.get('/', hotelController.show);
-router.delete('/', hotelController.delete);
-router.post('/update/', hotelController.update);
+router.delete('/', authenticated, hotelController.delete);
+router.post('/update/', authenticated, hotelController.update);
 
 module.exports = router;
--- server/routes/place.js
+++ server/routes/place.js
@@ -4,11 +4,12 @@ var path = require('path');
 var express = require('express');
 var router = express.Router();
 var placeController  = require('../controllers/place');
+var authenticated = require('./authenticated');
 
 console.log('In Place route===>');
-router.post('/', placeController.create);
-router.get('/', placeController.show);
-router.get('/all/', placeController.index);
-router.delete('/', placeController.delete);
-router.post('/update/', placeController.update);
+router.post('/', authenticated, placeController.create);
+router.get('/', authenticated, placeController.show);
+router.get('/all/', authenticated, placeController.index);
+router.delete('/', authenticated, placeController.delete);
+router.post('/update/', authenticated, placeController.update);
 module.exports = router;
