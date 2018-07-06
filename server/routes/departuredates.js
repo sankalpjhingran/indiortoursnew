3b71be1fdcc0296b528b2897fe95029ebbfc4cb3
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2018-07-05 20:54:33 -0700

    Update routes

--- server/routes/departuredates.js
+++ server/routes/departuredates.js
@@ -6,7 +6,6 @@ var router = express.Router();
 var departureCostsController  = require('../controllers/dates');
 
 console.log('In departureCostsController route===>');
-<<<<<<< HEAD
 
 /*
 ===Main Routes Start===
@@ -20,19 +19,17 @@ router.get('/', departureCostsController.show);
 /*
 ===Admin Routes Start===
 */
-router.post('/update/', authenticated, departureCostsController.update);
-router.delete('/', authenticated, departureCostsController.delete);
-router.post('/', authenticated, departureCostsController.create);
+router.post('/update/', departureCostsController.update);
+router.delete('/', departureCostsController.delete);
+router.post('/', departureCostsController.create);
 router.get('/all/', departureCostsController.index);
 /*
 ===Admin Routes End===
 */
-=======
 router.post('/', departureCostsController.create);
 router.get('/', departureCostsController.show);
 router.get('/all/', departureCostsController.index);
 router.delete('/', departureCostsController.delete);
 router.post('/update/', departureCostsController.update);
->>>>>>> parent of 3573d33... Add server side security for routes
 
 module.exports = router;
