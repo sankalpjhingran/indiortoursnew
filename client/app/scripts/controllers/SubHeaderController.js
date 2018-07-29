1d0ecd253edda7c60490f6b8c741f4c8122487cf
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2018-07-29 00:14:32 -0700

    Do search on enter Dont search if search text is blank

--- client/app/scripts/controllers/SubHeaderController.js
+++ client/app/scripts/controllers/SubHeaderController.js
@@ -8,7 +8,7 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('SubHeaderController', ['$http','$state', '$rootScope', '$scope', function ($http, $state, $rootScope, $scope) {
+  .controller('SubHeaderController', ['$http','$state', '$rootScope', '$scope', '$window', function ($http, $state, $rootScope, $scope, $window) {
     $scope.allImagesForBanner = function(){
       console.log('Calling subheader controller=====>');
       $scope.bannerImages = [];
@@ -24,19 +24,9 @@ angular.module('clientApp')
             }
        });
     }
-    $scope.searchTours = function() {
-        console.log('In searchTours function....');
-        $http.get('/api/search/', {params: {key: $scope.key}})
-         .then(function(response){
-              if(response.data.length){
-                  console.log(response.data);
-                  /*
-                  angular.forEach(response.data, function(image){
-                        console.log(image);
-                        $scope.bannerImages.push(image);
-                  });
-                  */
-              }
-         });
+    $scope.goToSearch = function() {
+      if($scope.key) {
+          $window.location.href = '/search?key=' + $scope.key;
+      }
     }
   }]);
