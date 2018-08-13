47714c9c85f79b9cfb8918351096476590946ece
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2018-08-13 12:15:21 -0700

    Add success message for Enquiry modal on submit.

--- client/app/scripts/controllers/ContactusController.js
+++ client/app/scripts/controllers/ContactusController.js
@@ -10,6 +10,7 @@
 angular.module('clientApp')
 .controller('ContactusController', function ($scope, $uibModal, $http, $location, $document, $log, Upload, $timeout) {
       console.log('In contact us controller');
+      $scope.showSuccessBanner = false;
       $scope.createLead = function(){
         $scope.contactusData.plannedarrival = moment($scope.contactusData.plannedarrival).toDate();
         $http.post('/api/contactus/', $scope.contactusData).then(function(res, err){
@@ -21,30 +22,45 @@ angular.module('clientApp')
         });
       }
 
+      $scope.createEnquiry = function(){
+        $scope.contactusData.plannedarrival = moment($scope.contactusData.plannedarrival).toDate();
+        $http.post('/api/contactus/', $scope.contactusData).then(function(res, err){
+          console.log(res);
+          if(res.status == 200){
+            $scope.uploadFiles(res.data);
+            $scope.showSuccessBanner = true;
+            setTimeout(function() {
+              $scope.cancel();
+            }, 2000);
+          }
+        });
+      }
+
       $scope.uploadFiles = function(tempContactusData) {
         var files = $scope.contactusData.images;
-        console.log($scope.contactusData.images);
-        angular.forEach(files, function(file) {
-            file.upload = Upload.upload({
-                url: '/api/image/',
-                method: 'POST',
-                data: { file: file,
-                        'parentobjectid': tempContactusData.id,
-                        'parentobjectname':  'lead'
-                      }
-            });
+        if(files) {
+          angular.forEach(files, function(file) {
+              file.upload = Upload.upload({
+                  url: '/api/image/',
+                  method: 'POST',
+                  data: { file: file,
+                          'parentobjectid': tempContactusData.id,
+                          'parentobjectname':  'lead'
+                        }
+              });
 
-            file.upload.then(function (response) {
-                $timeout(function () {
-                    file.result = response.data;
-                });
-            }, function (response) {
-                if (response.status > 0)
-                    $scope.errorMsg = response.status + ': ' + response.data;
-            }, function (evt) {
-                file.progress = Math.min(100, parseInt(100.0 *
-                                         evt.loaded / evt.total));
-            });
-        });
+              file.upload.then(function (response) {
+                  $timeout(function () {
+                      file.result = response.data;
+                  });
+              }, function (response) {
+                  if (response.status > 0)
+                      $scope.errorMsg = response.status + ': ' + response.data;
+              }, function (evt) {
+                  file.progress = Math.min(100, parseInt(100.0 *
+                                           evt.loaded / evt.total));
+              });
+          });
+        }
       }
 });
