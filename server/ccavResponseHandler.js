0b5cfc943538eddf126123b4d088e07e7bbbc7a0
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2018-08-23 11:29:16 -0700

    Fix images

---
+++ server/ccavResponseHandler.js
@@ -0,0 +1,30 @@
+var http = require('http'),
+    fs = require('fs'),
+    ccav = require('./ccavutil.js'),
+    qs = require('querystring');
+
+exports.postRes = function(request,response){
+    var ccavEncResponse='',
+	ccavResponse='',
+	workingKey = 'B0757E60598DE9B6CFB724211F06C60C',	//Put in the 32-Bit key shared by CCAvenues.
+	ccavPOST = '';
+
+      request.on('data', function (data) {
+	    ccavEncResponse += data;
+	    ccavPOST =  qs.parse(ccavEncResponse);
+	    var encryption = ccavPOST.encResp;
+	    ccavResponse = ccav.decrypt(encryption,workingKey);
+        });
+
+	request.on('end', function () {
+	    var pData = '';
+	    pData = '<table border=1 cellspacing=2 cellpadding=2><tr><td>'
+	    pData = pData + ccavResponse.replace(/=/gi,'</td><td>')
+	    pData = pData.replace(/&/gi,'</td></tr><tr><td>')
+	    pData = pData + '</td></tr></table>'
+            htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>'+ pData +'</center><br></body></html>';
+            response.writeHeader(200, {"Content-Type": "text/html"});
+	    response.write(htmlcode);
+	    response.end();
+	});
+};
