87aa608ab2787a3f228280c851c7d7e29173d8de
Sankalp Jhingran <sankalp.jhingran@salesforce.com>
2019-05-30 15:00:17 -0700

    Add video links and maps, images to Tourview page.

---
+++ server/migrations/11916302-add-video-links-field-to-tour.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+      */
+      return queryInterface.addColumn(
+        'Tours', // name of Target model
+        'videolink1', // name of the key we're adding
+        {
+          type: Sequelize.STRING,
+        },
+        'videolink2', // name of the key we're adding
+        {
+          type: Sequelize.STRING,
+        },
+        'videolink3', // name of the key we're adding
+        {
+          type: Sequelize.STRING,
+        }
+      );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
---
+++ server/migrations/11916304-add-video-links-field-to-tour.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+      */
+      return queryInterface.addColumn(
+        'Tours', // name of Target model
+        'videolink3', // name of the key we're adding
+        {
+          type: Sequelize.STRING,
+        }
+      );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
