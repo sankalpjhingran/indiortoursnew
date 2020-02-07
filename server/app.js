d1455682acd4b9147dedafc03b2ec7fca0097ed8
Sankalp Jhingran <sankalp.jhingran@gmail.com>
2020-02-07 15:11:27 -0800

    Fix parent tours

--- server/app.js
+++ server/app.js
@@ -135,7 +135,6 @@ app.use(logger('production'));
 //app.use('/api/tours/update', ensureAuthenticated, tours);
 
 app.use('/api/tournotes/update', ensureAuthenticated, notes);
-app.use('/api/parenttours/update', ensureAuthenticated, parenttours);
 app.use('/api/regusers', ensureAuthenticated, regusers);
 
 // Authenticated update paths end
@@ -209,10 +208,13 @@ app.use('/api/places', places);
 app.use('/api/places/all', places);
 app.use('/api/tags/all', tags);
 app.use('/api/tags', tags);
-app.use('/api/parenttours/all', parenttours);
-app.use('/api/parenttours/allTripsByOrder', parenttours);
-app.use('/api/parenttours', parenttours);
-app.use('/api/parenttours/viewtrip', parenttours);
+
+//app.use('/api/parenttours/all', parenttours);
+//app.use('/api/parenttours/allTripsByOrder', parenttours);
+//app.use('/api/parenttours', parenttours);
+//app.use('/api/parenttours/update', parenttours);
+//app.use('/api/parenttours/viewtrip', parenttours);
+
 app.use('/api/search', search);
 
 app.get('/api/conversionrates', function(request, res) {
