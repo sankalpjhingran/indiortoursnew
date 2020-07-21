f5cd6d206cbd5a42e5aa7d775fc0c66549f160c9
Sankalp Jhingran <31091638+sankalpjhingran@users.noreply.github.com>
2020-07-21 10:12:01 -0700

    Update jpg to webp (#425)
    * update jpgs to web
    
    * stop-renaming-of-files

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -249,10 +249,10 @@ module.exports = function (grunt) {
     filerev: {
       dist: {
         src: [
-          '<%= yeoman.dist %>/scripts/{,*/}*.js',
-          '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-          '<%= yeoman.dist %>/styles/fonts/*'
+          //'<%= yeoman.dist %>/scripts/{,*/}*.js',
+          //'<%= yeoman.dist %>/styles/{,*/}*.css',
+          //'<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          //'<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
     },
